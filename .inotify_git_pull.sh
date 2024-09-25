01dcf3f2f17a4e6c39880802fd1a8d01636e13ec
AndreaDonato <donato.1808606@studenti.uniroma1.it>
2024-09-25 23:06:56 +0200

    Automatic Commit

--- .inotify_git_pull.sh
+++ .inotify_git_pull.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Monitorare la cartella per l'accesso (read) e eseguire git pull
-inotifywait -m -e access /home/shaytaan/Desktop/int\ main/ComputerScience | while read -r directory events filename; do
-    /home/shaytaan/Desktop/int\ main/ComputerScience/.git_pull.sh
+# Inizializza la variabile per tenere traccia dell'ultimo esecuzione
+last_run=0
+
+inotifywait -m -r -e modify,close_write,create,delete /home/shaytaan/Desktop/int\ main/ComputerScience | while read -r directory events filename; do
+    current_time=$(date +%s)  # Ottieni il tempo attuale in secondi dall'epoca
+    # Controlla se sono trascorsi 600 secondi (10 minuti) dall'ultima esecuzione
+    if (( current_time - last_run >= 600 )); then
+        /home/shaytaan/Desktop/int\ main/ComputerScience/.git_pull.sh
+        last_run=$current_time  # Aggiorna il tempo dell'ultima esecuzione
+    fi
 done
