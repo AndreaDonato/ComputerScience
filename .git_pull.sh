bdfe2efb4696144e359acafb7151dcb42b419f98
AndreaDonato <donato.1808606@studenti.uniroma1.it>
2024-10-16 20:31:30 +0200

    Automatic Commit

--- .git_pull.sh
+++ .git_pull.sh
@@ -2,3 +2,57 @@
 
 cd /home/shaytaan/Desktop/int\ main/ComputerScience/ || exit
 git pull
+
+if [ $? -ne 0 ]; then
+    # Se c'è stato un errore, mostra una finestra con Zenity con tre opzioni
+    scelta=$(zenity --list --title="Errore" \
+        --text="Si è verificato un errore con il pull automatico, che succede?" \
+        --column="Opzioni" "Riprova un pull automatico" "Apri un terminale per visualizzare l'errore" "Ignora l'errore" "Ferma il pull automatico fino al prossimo riavvio" --height=250 --width=300)
+
+    case $scelta in
+        "Riprova un pull automatico")
+            echo "Riprovo il pull..."
+            ./.git_pull.sh
+            ;;
+        "Apri un terminale per visualizzare l'errore")
+
+			# Visto che bash è un po' limitato, per aspettare un terminale aperto tramite gnome-terminal bisogna fare così:
+			pid_file="/tmp/git_pull_terminal.pid"				# Creo un file temporaneo in cui salvare il pid del nuovo terminale
+
+			# Eseguo un terminale che stampa il suo stesso PID sul file di cui sopra, poi fa quello che deve fare
+			gnome-terminal -- bash -c "echo \$\$ > '$pid_file'; cd /home/shaytaan/Desktop/int\ main/ComputerScience/ || exit; git pull; exec bash" &
+			sleep 1 											# Gli serve un momento per fare tutta sta roba, il processore c'ha da fa
+			if [ -f "$pid_file" ]; then							# Se il file temporaneo esiste
+    			terminal_pid=$(<"$pid_file")       				# Leggi il PID dal file
+
+    			# La syscall wait non funziona se il processo da aspettare non è direttamente tuo figlio, quindi...
+				while true; do
+    				if ! ps -p $terminal_pid > /dev/null; then
+        				break  # Esci dal ciclo se il terminale è chiuso
+    				fi
+    				sleep 1  # Aspetta 1 secondo prima di controllare di nuovo
+			done
+
+			rm -f "$pid_file"									# Rimuovi il file temporaneo
+			else
+    			echo "Impossibile trovare il file $pid_file."	# Non si sa mai
+			fi
+			;;
+        "Ignora l'errore")
+            echo "Ignoro l'errore e proseguo..."
+            ;;
+        "Ferma il pull automatico fino al prossimo riavvio")
+      	  	pkexec bash -c 'systemctl stop CS-git-pull.service && systemctl stop CS-git-pull-inotify.service'
+      	  	if [[ $? -ne 0 ]]; then
+            	zenity --notification --text="Non è stato possibile interrompere il pull automatico."
+            else
+   	            echo "Disabilito il pull automatico..."
+      	  	fi
+            exit 1
+            ;;
+        *)
+            zenity --notification --text="Questa scelta equivale a selezionare 'Ignora l'errore'.\nNon ignorare i menù a tendina per favore."
+            exit 1
+            ;;
+    esac
+fi
