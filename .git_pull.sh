cecced1f8743767a44f47a489ef479747dc75a61
AndreaDonato <donato.1808606@studenti.uniroma1.it>
2024-10-17 18:41:15 +0200

    Automatic Commit

--- .git_pull.sh
+++ .git_pull.sh
@@ -13,10 +13,9 @@ sudo -u shaytaan xhost +local:root
 # Consenti a shaytaan di vedere le notifiche di zenity
 xhost +local:shaytaan
 
-#export DBUS_SESSION_BUS_ADDRESS=$(sudo -u shaytaan bash -c 'echo $DBUS_SESSION_BUS_ADDRESS')
-#export XDG_RUNTIME_DIR=$(sudo -u shaytaan bash -c 'echo $XDG_RUNTIME_DIR')
-
-export $(sudo -u shaytaan dbus-launch --sh-syntax)
+eval $(sudo -u shaytaan dbus-launch --sh-syntax)
+export DBUS_SESSION_BUS_ADDRESS
+export XDG_RUNTIME_DIR=$(sudo -u shaytaan bash -c 'echo $XDG_RUNTIME_DIR')
 
 git pull
 
